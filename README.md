c107500a3c371d365e4c8ff4000d37bcf46cb40f
floresenhalloween <fbientrigo@gmail.com>
2024-07-28 21:13:22 -0400

    initial commit
    A basic implementation of the fastAPI in which to build

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# mlAPI24
-FastAPI development for deploying tensorflow models 
+
+
+Instalar el environment
+```
+python -m venv env
+pip install -r requirements.txt
+```
+
+
+La forma de comenzar el server:
+```
+uvicorn main:app --reload
+```
---
+++ main.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI application!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
